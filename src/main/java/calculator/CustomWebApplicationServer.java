143c50a0c31df7e1ec1b859c663d38aaf5e8671c
ISMINIMIN <summmm.224@gmail.com>
2024-01-08 19:23:17 +0900

    feat: handle user requests in separate Threads
    Modify the application to process user requests in separate Threads to address blocking issues.

--- src/main/java/calculator/CustomWebApplicationServer.java
+++ src/main/java/calculator/CustomWebApplicationServer.java
@@ -28,28 +28,8 @@ public class CustomWebApplicationServer {
             while((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomWebApplicationServer] client connected!");
 
-                // 사용자 요청을 메인 Thread가 처리
-                try(InputStream in = clientSocket.getInputStream(); OutputStream out = clientSocket.getOutputStream()) {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    if(httpRequest.isGetRequest() && httpRequest.matchPath("/calculate")) {
-                        QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                        int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                        String operator = queryStrings.getValue("operator");
-                        int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int result = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(result).getBytes();
-
-                        HttpResponse httpResponse = new HttpResponse(dos);
-                        httpResponse.response200Header("application/json", body.length);
-                        httpResponse.responseBody(body);
-                    }
-                }
+                // 사용자 요청이 들어올 때마다 Thread 생성
+                new Thread(new ClientRequestHandler(clientSocket)).start();
             }
         }
     }
